35b27ab1f6d06884631db8991cb0093e697a3a3e
ibraphem <ibraphem@gmail.com>
2023-05-07 11:19:33 +0100

    base switch

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://localhost:7000/api';
-// export const BASE_URL = "https://florahomes-backend.herokuapp.com/api"
+// export const BASE_URL = 'http://localhost:7000/api';
+export const BASE_URL = "https://florahomes-backend.herokuapp.com/api"
